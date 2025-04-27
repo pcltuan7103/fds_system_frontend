c1a8d72b1deebce90101b4df2e98d1634d0a42a8
pcltuan7103 <tuanpcl7103@gmail.com>
2025-04-27 16:56:22 +0700

    [ADD]

--- src/components/Elements/Crisp.tsx
+++ src/components/Elements/Crisp.tsx
@@ -6,8 +6,12 @@ const CrispChat = () => {
     script.src = 'https://client.crisp.chat/l.js';
     script.async = true;
     script.onload = () => {
+      console.log("Crisp script loaded successfully!");  // Kiểm tra script có tải thành công không
       window.$crisp = [];
       window.CRISP_WEBSITE_ID = '318f2cb4-43da-4337-a75e-3f2abfc6e101'; // Thay "your_website_id" bằng ID của bạn
+    };
+    script.onerror = (error) => {
+      console.error("Error loading Crisp script: ", error);  // Kiểm tra lỗi nếu có
     };
     document.body.appendChild(script);
   }, []);
